9d9e3711df98ba7212e4eba7c0284baf2bcfd4a2
Yazan <yazanallahham552005@gmail.com>
2025-11-03 11:41:55 -0500

    Feat: Add missing backend routes for ORG-01 and ORG-04

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+const orgEventsRouter = require('./routes/orgEvents');
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -5,6 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/org/events', orgEventsRouter);  //added
 
 const PORT = process.env.PORT || 3000;
 
